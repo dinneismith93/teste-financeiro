d228927e713391a73d8eabbd5775e9bf9a65bd1a
dinneismith93 <dinneismith93@gmail.com>
2025-10-15 23:49:56 -0300

    Add files via upload

---
+++ script .js
@@ -0,0 +1,8 @@
+function iniciarTeste() {
+    // Redireciona para uma nova página de teste
+    window.location.href = "teste.html";
+    
+    // Ou se quiser um alerta primeiro para testar:
+    // alert("Teste iniciado com sucesso!");
+    // window.location.href = "teste.html";
+}
